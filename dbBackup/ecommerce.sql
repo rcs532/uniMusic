65600ad56c74cf53bd3b0195b2bb9344e397526f
rsalinas99 <ro-ca-sa@hotmail.com>
2021-03-05 16:17:10 -0600

    agregado backup database

--- dbBackup/ecommerce.sql
+++ dbBackup/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2021 at 12:01 AM
+-- Generation Time: Mar 05, 2021 at 11:15 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
